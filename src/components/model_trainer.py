4c69a5ca1006390981be2b11fa660326ea674289
imtiazhussain123 <imtiazh183@gmail.com>
2023-06-21 00:30:51 +0500

    Created prediction pipeline.

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,7 +1,7 @@
-import os 
+import os
 import sys
-
 from dataclasses import dataclass
+
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
@@ -16,38 +16,36 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object,evaluate_model
+
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join('artifacts','model.pkl')
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config = ModelTrainerConfig()
+        self.model_trainer_config=ModelTrainerConfig()
+
 
     def initiate_model_trainer(self,train_array,test_array):
         try:
-            logging.info("Split training and test input data.")
-            x_train,y_train,x_test, y_test = (
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
-                test_array[:,-1],
+                test_array[:,-1]
             )
-
             models = {
-                "Random Forest" : RandomForestRegressor(),
-                "Decision Tree" : DecisionTreeRegressor(),
-                "Gradient Boosting" : GradientBoostingRegressor(),
-                "Linear Regression" : LinearRegression(),
-                "K-Neighbor Classifier" : KNeighborsRegressor(),
-                "XGBClassifier" : XGBRegressor(),
-                "CatBoosting Classifier" : CatBoostRegressor(),
-                "AdaBoost classifier" : AdaBoostRegressor(),
-
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
             }
-
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -83,35 +81,39 @@ class ModelTrainer:
                     # 'loss':['linear','square','exponential'],
                     'n_estimators': [8,16,32,64,128,256]
                 }
+                
             }
 
-            model_report: dict = evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,
-                                                models= models,param = params)
-
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
+            
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
             ## To get best model name from dict
+
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-
             best_model = models[best_model_name]
 
-            if best_model_score < 0.6:
+            if best_model_score<0.6:
                 raise CustomException("No best model found")
-            logging.info("Best model found on both training and testing datasets")
+            logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
-                file_path = self.model_trainer_config.trained_model_file_path,
-                obj= best_model
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
             )
 
-            predicted = best_model.predict(x_test)
-            
-            r2_square = r2_score(y_test,predicted)
-            return r2_square
+            predicted=best_model.predict(X_test)
 
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+            
+
+
+
+            
         except Exception as e:
             raise CustomException(e,sys)
-        
